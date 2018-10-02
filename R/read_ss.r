b4de206ec1ab0743c1f676fe9496ce034c5cde42
Martin Pastoors <martin.pastoors@gmail.com>
2018-10-02 10:34:04 +0200

    read stock synthesis

--- R/read_ss.r
+++ R/read_ss.r
@@ -5,30 +5,74 @@
 # 
 # 03/09/2017 small modifications (MP)
 # 07/02/2018 added short term forecast exploration because of PELAC meeting
-# 01/09/2018 converted to tidy format
+# 01/09/2018 tried to convert to tidy format
+# 07/09/2018 back to using the FLStock intermediate RData file
 # ----------------------------------------------------------------------------------------------
 
 rm(list=ls())
 
+library(FLCore)
 library(r4ss)
 library(tidyverse)
 
 # source("R/SS3toFLStock_function.r")
 source("R/SS_readdat_3.30.r")
 source("R/SS_readctl_3.30.r")
+source("R/SS3toFLStock_v3.30.r")
 
-path     <- "D:/WGWIDE/2018 Meeting Docs/06. Data/hom.27.2a4a5b6a7a-ce-k8/assess" 
-datafile <- "data.ss_new"
-ctrlfile <- "control.ss_new"
+# Load FLStock converters
+source("../fishvise/R/converter.R")
+
+path         <- "D:/WGWIDE/2018/06. Data/hom.27.2a4a5b6a7a-ce-k8" 
+datafile     <- "data.ss_new"
+ctrlfile     <- "control.ss_new"
+wtatagefile  <- "wtatage.ss_new"
+rdatafile    <- "WHOM_SS3results.rdata"
+
+dat <- readLines(file.path(path, "output", wtatagefile),warn=FALSE)
+
+
+header <- strsplit(dat[8]," ")[[1]]
+data   <- strsplit(dat[9:length(dat)]," ")
+str(data)
+
 
 # Read the input data
 t <- 
-  SS_readdat_3.30(file.path(path, datafile)) 
+  SS_readdat_3.30(file.path(path, "output", datafile)) 
 
-# debug(SS_readctl_3.30)
+# Read the control file
 c <-
-  SS_readctl_3.30(file.path(path, ctrlfile))
-# undebug(SS_readctl_3.30)
+  SS_readctl_3.30(file.path(path, "output", ctrlfile))
+
+# load the rdata file
+dat <- 
+  get(load(file.path(path, "output", rdatafile)))
+
+# create rbya data.frame from FLStock object - this is a shortcut; should be done directly from input files
+rbya  <- 
+  FLStock2rbya(dat) %>% 
+  mutate(
+    stock = "hom.27.2a4a5b6a7a-ce-k8",
+    assessmentyear= 2018)
+
+write.csv(rbya, file=file.path(path,"output","tidy", "rbya.csv"), row.names = FALSE)
+# write.csv(rby,  file=file.path(savepath, "rby.csv"), row.names = FALSE)
+
+library(icesSAG)
+
+# use token
+options(icesSAG.use_token = TRUE)
+
+assessmentkey <- max(findAssessmentKey("hom.27.2a4a5b6a7a-ce-k8"))
+
+# getStockDownloadData(7306)
+
+# Download all the stock data - takes a long time
+t1  <- getStockDownloadData(assessmentkey)
+
+
+# Not finalized below 
 
 # get summary data
 fy        <- t$styr
@@ -46,7 +90,8 @@ oC <-
   dplyr::select(year=Yr, 
                 names(.)[grepl("^a", names(.))]) %>% 
   tidyr::gather(key="age", value="oC", a0:a15) %>% 
-  dplyr::mutate(age = as.integer(gsub("a","", age)))
+  dplyr::mutate(age = as.integer(gsub("a","", age))) %>% 
+  dplyr::arrange(year, age)
 
 # catch weight and stock weight
 if (t[["use_meanbodywt"]] == 0) {
@@ -61,45 +106,6 @@ m <- data.frame(expand.grid(year = fy:ly, age=fa:la), m = 0.15, stringsAsFactors
 # maturity
 mat <- 0.1
 
-str(t)
-  
-
-names(t)
-str(t)
-
-# 2017 not in the datafile?
 
 
-names((oC[grepl("^a", names(oC))]))
-select_(.dots = v)
-
-#-----------------------------------------------------------------------
-# Creat and read FLStock object(s)
-#-----------------------------------------------------------------------
-
-dirSS3output <- path; stockname<-"WHmackerel"; fbar_amin<-1; fbar_amax<-10 
-  
-# stockname
-stockname <- "WHmackerel"   # SS3 dynamics, 1 morph per year, quarterly time step (Rec in Q1)
-
-# fbar ages
-fbar_amin <- 1; fbar_amax <- 10
-
-# Create an FLStock RData file and write to the standard path
-SS3toFLStock(dirSS3output = path, 
-             stockname    = stockname, 
-             fbar_amin    = fbar_amin, 
-             fbar_amax    = fbar_amax)
-
-# Load the data
-WHOM_2017  <- get(load("WHmackerel_SS3results.rdata"))
-
-
-comp <- FLStocks(new_assessment = WHOM_2017, benchmark = WHOM_bench)
-plot(comp)
-
-# create data.frames
-WHOM2017df  <- FLStock2rbya(WHOM_2017) %>% mutate(assess="WHB2017")
-
-rbya <- rbind(WHOM2017df, WHOMbenchdf)
 
