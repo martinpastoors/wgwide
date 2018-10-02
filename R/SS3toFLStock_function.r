3bb1dc73e04e7b8fc5c9ba23440491f627116942
Martin Pastoors <martin.pastoors@gmail.com>
2018-10-02 10:37:03 +0200

    SS3 to FLStock

--- R/SS3toFLStock_function.r
+++ R/SS3toFLStock_function.r
@@ -2,7 +2,25 @@
 function (dirSS3output, stockname, fbar_amin, fbar_amax) 
 {
 
-output <- SS_output(dir = dirSS3output, model = "ss3",repfile = "Report.sso", compfile = "CompReport.sso", covarfile = "covar.sso", ncols = 200, forecast = TRUE, warn = TRUE,covar = FALSE, checkcor = TRUE, cormax = 0.95, cormin = 0.01,printhighcor = 10, printlowcor = 10, verbose = TRUE,printstats = TRUE, hidewarn = FALSE, NoCompOK = FALSE,aalmaxbinrange=0)
+output <- SS_output(dir = dirSS3output, 
+                    model = "ss3",
+                    repfile = "Report.sso", 
+                    compfile = "CompReport.sso", 
+                    covarfile = "covar.sso", 
+                    ncols = 200, 
+                    forecast = TRUE, 
+                    warn = TRUE,
+                    covar = FALSE, 
+                    checkcor = TRUE, 
+                    cormax = 0.95, 
+                    cormin = 0.01,
+                    printhighcor = 10, 
+                    printlowcor = 10, 
+                    verbose = TRUE,
+                    printstats = TRUE, 
+                    hidewarn = FALSE, 
+                    NoCompOK = FALSE,
+                    aalmaxbinrange=0)
 
 
 ###############################################################
